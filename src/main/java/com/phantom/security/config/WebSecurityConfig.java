b191c64a5518fc03325e7f32eea3414dcd71aeb7
lei tan <leitan@leideMacBook-Pro.local>
2023-02-10 16:03:26 +0800

    项目初始化提交

--- src/main/java/com/phantom/security/config/WebSecurityConfig.java
+++ src/main/java/com/phantom/security/config/WebSecurityConfig.java
@@ -80,20 +80,6 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-
-    //    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        UserDetails user1 = User.withUsername("user")
-                // 密码明文 password
-                .password("{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG")
-                .roles("USER")
-                .build();
-        manager.createUser(user1);
-        return manager;
-    }
-
-
     @Bean
     public AuthenticationManager authenticationManager() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
